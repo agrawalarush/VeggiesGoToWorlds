b5f5de20a4d8e0a29c8f799a736898c7031f69b7
agrawalarush <connectarushxyz@gmail.com>
2026-04-11 11:36:36 -0700

    Initial commit

---
+++ VeggieWorlds/include/robotConfigs.h
@@ -0,0 +1,13 @@
+#ifndef _PROS_ROBOT_CONFIG_H_
+#define _PROS_ROBOT_CONFIG_H_
+#include "main.h"
+#include "lemlib/api.hpp"
+
+extern pros::MotorGroup left_motor_group;
+extern pros::MotorGroup right_motor_group;
+
+extern lemlib::Chassis chassis;
+
+extern pros::Controller controller;
+
+#endif
